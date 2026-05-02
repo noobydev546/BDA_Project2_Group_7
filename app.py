f382b3dbbef1a98c99203d2875d3c6f7fef7ec1b
noobydev546 <donutaaaa19@gmail.com>
2026-05-02 19:29:29 +0700

    Fix import path for create_retrieval_chain

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 # 1. Page Configuration
